a42c6d6518d2ecca7dadf2ac63786ba4f5fd0146
bokyeong96 <qhrud9399@naver.com>
2025-04-15 17:21:31 +0900

    질문 미리보기 영역 구현 중

---
+++ src/components/PreviewSection/index.js
@@ -0,0 +1,13 @@
+import Card from '../Card';
+
+function PreviewSection() {
+  return (
+    <>
+      <Card title="title" desc="desc">
+        Body
+      </Card>
+    </>
+  );
+}
+
+export default PreviewSection;
--- src/pages/BuilderPage.js
+++ src/pages/BuilderPage.js
@@ -1,7 +1,23 @@
+import { Col, Input, Row } from 'antd';
+
+import OptionSection from '../components/OptionSection';
+import PreviewSection from '../components/PreviewSection';
 import MainLayout from '../layouts/MainLayOut';
 
 function BuilderPage() {
-  return <MainLayout selectedKeys={['builder']}>BuilderPage</MainLayout>;
+  return (
+    <MainLayout selectedKeys={['builder']}>
+      <Row>
+        <Col flex="auto">
+          <Input placeholder="설문 제목을 입력해주세요." />
+          <PreviewSection />
+        </Col>
+        <Col flex="350px">
+          <OptionSection />
+        </Col>
+      </Row>
+    </MainLayout>
+  );
 }
 
 export default BuilderPage;
